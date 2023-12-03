18a3aaa8c80fddd8285d261a56c50336419486c3
Rahul Kumar Saw <2021chb1052@iitrpr.ac.in>
2023-12-03 16:04:25 +0530

    port changes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import teamRouter from "./routes/teamRoutes.js";
 const app = express();
 config({path: './.env'});
 
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +25,6 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
